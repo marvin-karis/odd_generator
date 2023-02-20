162a487a0fe20f521c577ec5750318c39f7fefe6
marvin-karis <marvinirungu58@gmail.com>
2023-02-20 21:41:07 +0300

    to date"

--- OddGen.py
+++ OddGen.py
@@ -1,39 +1,36 @@
 import requests
+import json
 from sklearn.naive_bayes import MultinomialNB
 import numpy as np
 
 # Prompt for input of two team names
-homeTeam = input("Enter the name of first football team: ")
-awayTeam = input("Enter the name of second football team: ")
+homeTeam = int(input("Enter the id of first football team:"))
+awayTeam = int(input("Enter the id of second football team:"))
 
-# API endpoint to get head to head match results between two teams
-url = f"https://api-football-v1.p.rapidapi.com/fixtures/h2h?homeTeam={homeTeam}&awayTeam={awayTeam}"
+# API endpoint to get head to head match results between two team
+url = "https://v3.football.api-sports.io/fixtures/headtohead"
+
+querystring = {"h2h" : f"{homeTeam}-{awayTeam}", "last" : "3"}
+querrystring = json.dumps(querystring, separators=('=' '-'))
 
 headers = {
-    "X-RapidAPI-Key": "####YOUR_API_KEY######",
-    "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
+	"X-RapidAPI-Key": "############YOUR API KEY#############",
+	"X-RapidAPI-Host": "v3.football.api-sports.io"
 }
 
-# Make a request to the API
-response = requests.request("GET", url, headers=headers)
-
+responses = requests.request("GET", url, params=querystring,  headers=headers )
 # Parse the response data
-data = response.json()
-
-# Get the last 5 most recently head to head match results
-final_score = data["headToHeadResults"][:5]
-
+data = responses.json()
+#Get the last 5 most recently head to head match results
+final_score = data["response"]
+#print(final_score)
 # Print the head to head results
 for result in final_score:
-    print(
-        f"{result['homeTeam']} {result['homeTeamScore']} - {result['awayTeamScore']} {result['awayTeam']}")
-
+    print(f"{result['teams']['home']['name']} {result['goals']['home']} - {result['goals']['away']} {result['teams']['away']['name']}")
 
 # Create a dataset of match results
-X = np.array([[result["homeTeamScore"], result["awayTeamScore"]]
-             for result in final_score])
-y = np.array([1 if result["homeTeamScore"] >
-             result["awayTeamScore"] else 0 for result in final_score])
+X = np.array([[result['goals']['home'], result['goals']['away']] for result in final_score])
+y = np.array([1 if result['goals']['home'] > result['goals']['away'] else 0 for result in final_score])
 
 # Train a Naive Bayes classifier
 clf = MultinomialNB()
@@ -46,5 +43,5 @@ prob_homeTeam_winning = clf.predict_proba([[1, 0]])[0][1]
 prob_awayTeam_winning = clf.predict_proba([[0, 1]])[0][0]
 
 # Print the probabilities
-print(f"Probability of {homeTeam} winning: {prob_homeTeam_winning:.2f}")
-print(f"Probability of {awayTeam} winning: {prob_awayTeam_winning:.2f}")
+print(f"Probability of {0} winning: {prob_homeTeam_winning:.2f}")
+print(f"Probability of {awayTeam} winning: {prob_awayTeam_winning:.2f}") 
